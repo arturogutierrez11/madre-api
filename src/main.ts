7746edb7a49487b5bf90fd5dab05c9bda1a1de3a
arturogutierrez11 <agutierrez.gyo@gmail.com>
2025-12-22 18:20:09 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { setupSwagger } from './common/swagger/swagger.setup';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api');
+
+  setupSwagger(app, 'Madre API', 'API principal de Madre DB', ['madre api']);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  console.log(`API running on port ${process.env.PORT ?? 3000}`);
+}
+bootstrap();
